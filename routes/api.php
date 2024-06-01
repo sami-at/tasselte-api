d5e2da6408bf6692f31d975ae66ff1537bb81e28
sami-at <samiatlagh3@gmail.com>
2024-06-01 15:19:12 +0100

    hh

--- routes/api.php
+++ routes/api.php
@@ -19,3 +19,7 @@ use App\Http\Controllers\ProductController;
 Route::get('/products', [ProductController::class, 'index']);
 Route::get('/products/{id}', [ProductController::class, 'show']);
 
+Route::get('/test-online', function () {
+    dd('i am online ^_^');
+});
+
